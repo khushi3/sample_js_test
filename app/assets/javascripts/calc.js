5923e3974ede145923a26e2d510b159d88a9addf
Khushboo Bharti <khushboo.barathi@emids.com>
2015-04-16 10:54:39 -0800

    spec for newline delimiters

--- app/assets/javascripts/calc.js
+++ app/assets/javascripts/calc.js
@@ -2,7 +2,7 @@ var calc = {
     add: function (params) {
         if (params == 0)
             return 0
-        var numbers = params.split("/\n/")
+        var numbers = params.split(",")
         var result = 0;
         for (var i = 0; i < numbers.length; i++) {
             result += Number(numbers[i]);
