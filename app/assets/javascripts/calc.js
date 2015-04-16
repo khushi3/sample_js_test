ebd8d178182d808fda8f5d6b22b192ec02753cf1
Khushboo Bharti <khushboo.barathi@emids.com>
2015-04-15 17:21:40 -0800

    testing empty string

---
+++ app/assets/javascripts/calc.js
@@ -0,0 +1,6 @@
+var calc = {
+    add: function(params) {
+    if(params == 0)
+    return 0
+    }
+}
---
+++ spec/javascripts/calc.spec.js
@@ -0,0 +1,7 @@
+describe("calc",function(){
+   it("should support empty string",function() {
+       expect(calc.add("")).toEqual(0);
+   });
+    it("")
+});
+
