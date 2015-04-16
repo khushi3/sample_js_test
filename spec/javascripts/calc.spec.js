1c3efcc2d12d740cf37e074e6d9089ea80156058
Khushboo Bharti <khushboo.barathi@emids.com>
2015-04-16 10:58:03 -0800

    spec for supporting 1 number

--- spec/javascripts/calc.spec.js
+++ spec/javascripts/calc.spec.js
@@ -2,6 +2,9 @@ describe("calc",function(){
    it("should support empty string",function() {
        expect(calc.add("")).toEqual(0);
    });
+    it("should support  1 number ",function () {
+        expect(calc.add("3")).toEqual(3);
+    })
     it("should support more than 2 numbers ",function () {
      expect(calc.add("3,2,10")).toEqual(15);
     })
